245c0733f9d574eba85a37d07816621c852fd76d
nipanshu-mishra <114762078+nipanshu-mishra@users.noreply.github.com>
2023-05-03 16:37:06 +0530

    photo-gallery

---
+++ custom.js
@@ -0,0 +1,106 @@
+mainImg = document.getElementById('mainImg');
+
+thumb1 = document.getElementById('thumb1');
+thumb1Src = document.getElementById('thumb1').src;
+
+thumb2 = document.getElementById('thumb2');
+thumb2Src = document.getElementById('thumb2').src;
+
+thumb3 = document.getElementById('thumb3');
+thumb3Src = document.getElementById('thumb3').src;
+
+thumb4 = document.getElementById('thumb4');
+thumb4Src = document.getElementById('thumb4').src;
+
+thumb5= document.getElementById('thumb5');
+thumb5Src = document.getElementById('thumb5').src;
+
+thumb6 = document.getElementById('thumb6');
+thumb6Src = document.getElementById('thumb6').src;
+
+thumb7 = document.getElementById('thumb7');
+thumb7Src = document.getElementById('thumb7').src;
+
+thumb8 = document.getElementById('thumb8');
+thumb8Src = document.getElementById('thumb8').src;
+
+thumb9 = document.getElementById('thumb9');
+thumb9Src = document.getElementById('thumb9').src;
+
+thumb10 = document.getElementById('thumb10');
+thumb10Src = document.getElementById('thumb10').src;
+
+thumb11 = document.getElementById('thumb11');
+thumb11Src = document.getElementById('thumb11').src;
+
+thumb12 = document.getElementById('thumb12');
+thumb12Src = document.getElementById('thumb12').src;
+
+thumb13 = document.getElementById('thumb13');
+thumb13Src = document.getElementById('thumb13').src;
+
+thumb14 = document.getElementById('thumb14');
+thumb14Src = document.getElementById('thumb14').src;
+
+thumb15 = document.getElementById('thumb15');
+thumb15Src = document.getElementById('thumb15').src;
+
+thumb1.addEventListener("click", function(){
+    mainImg.src = thumb1Src;
+})
+
+thumb2.addEventListener("click", function(){
+    mainImg.src = thumb2Src;
+})
+
+thumb3.addEventListener("click", function(){
+    mainImg.src = thumb3Src;
+})
+
+thumb4.addEventListener("click", function(){
+    mainImg.src = thumb4Src;
+})
+
+thumb5.addEventListener("click", function(){
+    mainImg.src = thumb5Src;
+})
+
+thumb6.addEventListener("click", function(){
+    mainImg.src = thumb6Src;
+})
+
+thumb7.addEventListener("click", function(){
+    mainImg.src = thumb7Src;
+})
+
+thumb8.addEventListener("click", function(){
+    mainImg.src = thumb8Src;
+})
+
+thumb9.addEventListener("click", function(){
+    mainImg.src = thumb9Src;
+})
+
+thumb10.addEventListener("click", function(){
+    mainImg.src = thumb10Src;
+})
+
+thumb11.addEventListener("click", function(){
+    mainImg.src = thumb11Src;
+})
+
+thumb12.addEventListener("click", function(){
+    mainImg.src = thumb12Src;
+})
+
+thumb13.addEventListener("click", function(){
+    mainImg.src = thumb13Src;
+})
+
+thumb14.addEventListener("click", function(){
+    mainImg.src = thumb14Src;
+})
+
+thumb15.addEventListener("click", function(){
+    mainImg.src = thumb15Src;
+})
